8da6ba23bce0c871300d1ca7512137a99565c313
aastha-24 <aastha.priya@wynk.in>
2017-05-20 17:40:13 +0530

    minor changes

--- src/main/java/in/wynk/phoenix/handler/UserRegistrationHandler.java
+++ src/main/java/in/wynk/phoenix/handler/UserRegistrationHandler.java
@@ -2,6 +2,7 @@ package in.wynk.phoenix.handler;
 
 import in.wynk.netty.common.RequestMapping;
 import in.wynk.netty.common.ResponseType;
+import in.wynk.netty.handler.IBaseRequestHandler;
 import io.netty.handler.codec.http.HttpRequest;
 
 import java.util.List;
@@ -11,11 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller("/wynk/v1/registration.*")
-public class UserRegistrationHandler {
+public class UserRegistrationHandler implements IBaseRequestHandler {
 
-    @RequestMapping(value = "/wynk/v1/registration/login", method = RequestMethod.POST, responseType = ResponseType.JSON)
+    @RequestMapping(value = "/wynk/v1/registration/login", method = RequestMethod.POST, responseType = ResponseType.TEXT)
     public String makePayment(HttpRequest httprequest, Map<String, List<String>> urlParameters, String requestPayload) {
-        String sharedSecret = null;
+        String sharedSecret = "aastha";
         return sharedSecret;
     }
 
